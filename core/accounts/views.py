eabd36f3fe7e04793cf6a84a2163a72bc147832d
Arshia <arshiashirvani.f1385@gmail.com>
2024-11-02 00:34:22 +0330

    write urls and views and forms for login part

---
+++ core/accounts/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import views as auth_views
+from accounts.forms import AuthenticationForm
+
+class LoginView(auth_views.LoginView):
+    template_name = "accounts/login.html"
+    form_class = AuthenticationForm
+    redirect_authenticated_user = True
