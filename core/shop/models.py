5cedb5bad4a8fae00b5cf8ab6b19fd4cbecfee80
Arshia <arshiashirvani.f1385@gmail.com>
2024-11-04 01:06:22 +0330

    write model of the products and category and then write views of product list  finally migration

--- core/shop/models.py
+++ core/shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class ProductStatusType(models.IntegerChoices):
+    active = 1,("فعال")
+    disabled = 2,("غیرفعال")
+
+
+class ProductCategoryModel(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(allow_unicode = True,unique=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class ProductModel(models.Model):
+    user = models.ForeignKey("accounts.User",on_delete=models.PROTECT)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(allow_unicode = True,unique=True)
+    category = models.ManyToManyField(ProductCategoryModel)
+    image = models.ImageField(default="shop/default-image/default-product.png",upload_to="shop/upload-image/")
+    description = models.TextField()
+    stock = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10,decimal_places=0,default=0)
+    discount_percent = models.IntegerField(default=0)
+    status = models.IntegerField(choices=ProductStatusType.choices,default=ProductStatusType.disabled.value)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class ProductImageModel(models.Model):
+    Product = models.ForeignKey(ProductModel,on_delete=models.CASCADE)
+    file = models.ImageField(default="shop/default-image/default-product.png",upload_to="shop/extra-img/")
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (TemplateView,
+                                  ListView,
+                                  DetailView)
+from .models import (ProductModel,
+                     ProductCategoryModel,
+                     ProductStatusType)
 
-class ProductListView(TemplateView):
-    template_name = 'shop/product-list.html'
+
+class ProductListView(ListView):
+    template_name = 'shop/product-grid.html'
+    queryset = ProductModel.objects.filter(status=ProductStatusType.active.value)
