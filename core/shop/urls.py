28b1a4b6b2aeb8ee608ffc5c2c6319c3cdfbfc5c
Arshia <arshiashirvani.f1385@gmail.com>
2024-11-07 17:17:53 +0330

    retrieving a product with show all details

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from . import views
 
 app_name = 'shop'
 
 urlpatterns = [
     path('product-list',views.ProductListView.as_view(),name='product-list'),
+    re_path(r"product/detail/(?P<slug>[-\w]+)/",views.ProductDetailView.as_view(),name="product-detail"),
 ]
--- core/shop/views.py
+++ core/shop/views.py
@@ -11,9 +11,14 @@ from .models import (ProductModel,
 class ProductListView(ListView):
     template_name = 'shop/product-grid.html'
     queryset = ProductModel.objects.filter(status=ProductStatusType.active.value)
-    paginate_by = 2
+    paginate_by = 12
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
+    
+class ProductDetailView(DetailView):
+    template_name = "shop/product-detail.html"
+    queryset = ProductModel.objects.filter(status=ProductStatusType.active.value)
