cbccd76271c3360889780b96def37f1da80f2469
Arshia <arshiashirvani.f1385@gmail.com>
2024-11-18 22:56:04 +0330

    write session cart with this options ( add , delete , update , show) and create cart file for doing all of the operations of the cart

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -5,5 +5,7 @@ app_name = "cart"
 
 urlpatterns = [
     path('session/add-product/',views.SessionAddProductView.as_view(),name='session-add-product'),
-    path('session/cart/summary/',views.SessionCartSummary.as_view(),name='session-cart-summary'),
+    path('session/cart/summary/',views.SessionCartSummaryView.as_view(),name='session-cart-summary'),
+    path('session/cart/delete-product/',views.SessionDeleteProductView.as_view(),name='session-cart-delete-product'),
+    path('session/cart/update-product/',views.SessionUpdateQuantityProductView.as_view(),name='session-cart-update-product'),
 ]
--- core/cart/views.py
+++ core/cart/views.py
@@ -12,6 +12,35 @@ class SessionAddProductView(View):
             cart.add_product(product_id)
         return JsonResponse({"cart":cart.get_cart_dict(),"total_quantity":cart.get_total_quantity()})
     
-class SessionCartSummary(TemplateView):
+class SessionCartSummaryView(TemplateView):
     template_name = "cart/cart-summary.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = CartSession(self.request.session)
+        cart_items = cart.get_cart_items()
+        context["cart_items"] = cart_items
+        context["total_payment_price"] = cart.get_total_payment_amount()
+        context["total_quantity"] = cart.get_total_quantity()
+        return context
+    
+class SessionDeleteProductView(View):
+
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        if product_id:
+            cart.remove_product(product_id)
+            return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
+    
+
+class SessionUpdateQuantityProductView(View):
+
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        quantity = request.POST.get("quantity")
+        if product_id and quantity:
+            cart.update_product_quantity(product_id, quantity)
+        return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
         
