c3aebf620312fdf108b2e74ad5a603072c1fc629
Arshia <arshiashirvani.f1385@gmail.com>
2024-11-21 18:00:02 +0330

    add template of  the checkout and write views with permissions
    in the permissions said just customer can enter

--- core/order/views.py
+++ core/order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from order.permissions import HasCustomerAccessPermission
+    
+    
+    
+class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,TemplateView):
+    template_name = "order/checkout.html"
