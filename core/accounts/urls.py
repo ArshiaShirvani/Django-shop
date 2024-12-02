daa434bc4fc1905a3de91f153fc91c4084e306fb
Arshia <arshiashirvani.f1385@gmail.com>
2024-12-03 00:17:56 +0330

    Update urls.py

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login',views.LoginView.as_view(),name='login'),
     path('logout',views.LogoutView.as_view(),name='logout'),
+    # path('register',views.SignUpView.as_view(),name='register'),
     path('register',views.register_view,name='register'),
     path('complete-profile',views.complete_profile_view,name='complete-profile'),
 ]
