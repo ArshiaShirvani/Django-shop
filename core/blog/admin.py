9ebf18b420e345e1bc592c9c6e3e664fe96d1572
Arshia <arshiashirvani.f1385@gmail.com>
2024-12-16 13:55:33 +0330

    customize admin for blog

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import PostModel,PostCategory,CommentModel
 
-# Register your models here.
+@admin.register(PostModel)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id','title','created_date','status')
+    
+@admin.register(PostCategory)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('id','name','created_date')
+    
+@admin.register(CommentModel)
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ('post','name','email','message')
