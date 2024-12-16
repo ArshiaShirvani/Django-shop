af5437bde8a703e3b427f3fe7195b3be90de2698
Arshia <arshiashirvani.f1385@gmail.com>
2024-12-16 13:55:54 +0330

    write urls and views for blog

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
+from . import views
 
 app_name = "blog"
 
 urlpatterns = [
-    
+    path("post-list/",views.BlogPostListView.as_view(),name="post-list"),
+    re_path(r"post/detail/(?P<slug>[-\w]+)/",views.BlogPostDetailView.as_view(),name="post-detail"),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+)
+from blog.models import PostModel,PostStatusType,PostCategory
+from django.core.exceptions import FieldError
 
-# Create your views here.
+class BlogPostListView(ListView):
+    template_name = "blog/post-list.html"
+    paginate_by = 3
+    context_object_name = 'posts'
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size', self.paginate_by)
+    
+    def get_queryset(self):
+        queryset = PostModel.objects.filter(
+            status=PostStatusType.active.value)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id := self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()
+        context["categories"] = PostCategory.objects.all()
+        return context
+
+class BlogPostDetailView(DetailView):
+    template_name = "blog/post-single.html"
+    queryset = PostModel.objects.filter(status=PostStatusType.active.value)
