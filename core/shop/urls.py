ebed44976706dfed719a3871ab75baf9a68e6e5e
Arshia <arshiashirvani.f1385@gmail.com>
2025-01-11 22:51:01 +0330

    write urls for add or remove wishlists in product-list template or product-detail

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'shop'
 urlpatterns = [
     path('product-list/',views.ProductListView.as_view(),name='product-list'),
     re_path(r"product/detail/(?P<slug>[-\w]+)/",views.ProductDetailView.as_view(),name="product-detail"),
+    path("wishlist/add-or-remove",views.AddOrRemoveWishListView.as_view(),name="add-or-remove-wishlist"),
 ]
