444410aba72227607a040a5f77c99a38e0a3db05
Arshia <arshiashirvani.f1385@gmail.com>
2025-01-14 00:39:11 +0330

    write views of the review for product detail

--- core/shop/views.py
+++ core/shop/views.py
@@ -2,11 +2,17 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import (TemplateView,
                                   ListView,
-                                  DetailView)
+                                  DetailView,
+                                  View)
 from .models import (ProductModel,
                      ProductCategoryModel,
-                     ProductStatusType)
+                     ProductStatusType,
+                     ProductImageModel,
+                     WishListModel)
 from django.core.exceptions import FieldError
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from review.models import ReviewModel,ReviewStatusType
 
 class ProductListView(ListView):
     template_name = 'shop/product-grid.html'
@@ -36,9 +42,57 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
+        context["wishlist_items"] = WishListModel.objects.filter(user=self.request.user).values_list(
+            "product__id", flat=True) if self.request.user.is_authenticated else []
         context["categories"] = ProductCategoryModel.objects.all()
         return context
     
 class ProductDetailView(DetailView):
     template_name = "shop/product-detail.html"
     queryset = ProductModel.objects.filter(status=ProductStatusType.active.value)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        context["product_images"] = ProductImageModel.objects.filter(product__id=product.id)
+        context["is_wished"] = WishListModel.objects.filter(
+            user=self.request.user, product__id=product.id).exists() if self.request.user.is_authenticated else False
+        reviews = ReviewModel.objects.filter(product=product,status=ReviewStatusType.accepted.value)
+        context["reviews"] = reviews
+        total_reviews_count =reviews.count()
+        context["reviews_count"] = {
+            f"rate_{rate}": reviews.filter(rate=rate).count() for rate in range(1, 6)
+        }
+        if total_reviews_count != 0:
+            context["reviews_avg"] = {
+                f"rate_{rate}": round((reviews.filter(rate=rate).count()/total_reviews_count)*100,2) for rate in range(1, 6)
+            }
+        else:
+            context["reviews_avg"] = {f"rate_{rate}": 0 for rate in range(1, 6)}
+        return context
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.product_images.prefetch_related()
+        return obj
+    
+class AddOrRemoveWishListView(LoginRequiredMixin,View):
+    
+    def post(self,request,*args,**kwargs):
+        product_id = request.POST.get("product_id")
+        message = ""
+        
+        if product_id:
+            try:
+                wishlist_item = WishListModel.objects.get(user=request.user,product__id=product_id)
+                wishlist_item.delete()
+                message = "محصول از لیست علایق حذف شد"
+                
+            except WishListModel.DoesNotExist:
+                WishListModel.objects.create(user=request.user,product_id=product_id)
+                message = "محصول به لیست علایق اضافه شد"
+                
+        return JsonResponse({"message":message})
+                
+    
+    
